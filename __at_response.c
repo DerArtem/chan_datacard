1f15640ad568aca2c7e450ed3091c2d71ed70119
Artem Makhutov <artem@makhutov.org>
2010-06-23 05:18:22 +0000

    Fix ussd for some broken devices that do not report their model.
    git-svn-id: http://www.makhutov.org/svn/chan_datacard/trunk@118 2a160f14-7a49-45f3-a44f-b86709aeb494

--- __at_response.c
+++ __at_response.c
@@ -1424,17 +1424,21 @@ static inline int at_response_cgmi (pvt_t* pvt, char* str, size_t len)
 
 static inline int at_response_cgmm (pvt_t* pvt, char* str, size_t len)
 {
-	ast_copy_string (pvt->model, str, sizeof (pvt->model));
+	// lets initialize some default values
+	pvt->cusd_use_7bit_encoding = 0;
+	pvt->cusd_use_ucs2_decoding = 1;
 
-	if (!strcmp (pvt->model, "E1550") || !strcmp (pvt->model, "E160X"))
+	// some devices may be broken and no report their model
+	// some other devices require special treatment for ussd
+	if (pvt->model!=NULL)
 	{
-		pvt->cusd_use_7bit_encoding = 1;
-		pvt->cusd_use_ucs2_decoding = 0;
-	}
-	else
-	{
-		pvt->cusd_use_7bit_encoding = 0;
-		pvt->cusd_use_ucs2_decoding = 1;
+		ast_copy_string (pvt->model, str, sizeof (pvt->model));
+	
+		if (!strcmp (pvt->model, "E1550") || !strcmp (pvt->model, "E160X"))
+		{
+			pvt->cusd_use_7bit_encoding = 1;
+			pvt->cusd_use_ucs2_decoding = 0;
+		}
 	}
 
 	return 0;

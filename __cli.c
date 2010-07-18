b2e5e2492ae90d492eb53fdcf78103ad5eadf238
Artem Makhutov <artem@makhutov.org>
2010-07-18 17:14:57 +0000

    Show auto delete sms settings on datacard show device.
    git-svn-id: http://www.makhutov.org/svn/chan_datacard/trunk@149 2a160f14-7a49-45f3-a44f-b86709aeb494

--- __cli.c
+++ __cli.c
@@ -127,7 +127,8 @@ static char* cli_show_device (struct ast_cli_entry* e, int cmd, struct ast_cli_a
 		ast_cli (a->fd, "  USSD use 7 bit encoding : %s\n", pvt->cusd_use_7bit_encoding ? "Yes" : "No");
 		ast_cli (a->fd, "  USSD use UCS-2 decoding : %s\n", pvt->cusd_use_ucs2_decoding ? "Yes" : "No");
 		ast_cli (a->fd, "  Location area code      : %s\n", pvt->location_area_code);
-		ast_cli (a->fd, "  Cell ID                 : %s\n\n", pvt->cell_id);
+		ast_cli (a->fd, "  Cell ID                 : %s\n", pvt->cell_id);
+		ast_cli (a->fd, "  Auto delete SMS         : %s\n\n", pvt->auto_delete_sms ? "Yes" : "No");
 		ast_mutex_unlock (&pvt->lock);
 	}
 	else
--- __manager.c
+++ __manager.c
@@ -57,6 +57,7 @@ static int manager_show_devices (struct mansession* s, const struct message* m)
 		astman_append (s, "USSD use UCS-2 decoding: %s\r\n", pvt->cusd_use_ucs2_decoding ? "Yes" : "No");
 		astman_append (s, "Location area code: %s\r\n", pvt->location_area_code);
 		astman_append (s, "Cell ID: %s\r\n", pvt->cell_id);
+		astman_append (s, "Auto delete SMS: %s\r\n", pvt->auto_delete_sms ? "Yes" : "No");
 		astman_append (s, "\r\n");
 		ast_mutex_unlock (&pvt->lock);
 		count++;
